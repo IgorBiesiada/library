567311ef87b2d80878d9ed520b62e8d4c89a25a8
Igor <xxxigorbiesiada1@gmail.com>
2025-01-02 16:59:32 +0100

    installing packages adding models without migration

--- __init__.py
+++ __init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from extensions import db
 from config import Config
 from routes import init_routes
+from flask_migrate import Migrate
+
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(__name__)
+    app.config.from_object(Config)
 
     db.init_app(app)
 
     init_routes(app)
+    migrate = Migrate(app, db)
 
     with app.app_context():
         db.create_all()
---
+++ models.py
@@ -0,0 +1,24 @@
+from extensions import db
+
+class BookCategory(db.Model):
+    __tablename__ = 'book_category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __str__(self):
+        return self.name
+
+class Book(db.Model):
+    __tablename__ = 'book'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=False)
+    pages = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('book_category.id'), nullable=False)
+    category = db.relationship('BookCategory', backref=db.backref('books', lazy=True))
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
