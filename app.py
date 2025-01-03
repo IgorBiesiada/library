245b8ca09ad932ef375a1ce86fa9988efad48c97
Igor <xxxigorbiesiada1@gmail.com>
2025-01-03 17:20:33 +0100

    Removing the config file form the repository

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from routes import init_routes
 from flask_migrate import Migrate
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates', static_folder='static')
     app.config.from_object(Config)
 
     db.init_app(app)
 
     init_routes(app)
-    migrate = Migrate(app, db)
+    Migrate(app, db)
 
     with app.app_context():
         db.create_all()
--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'postgresql://postgres:1234@localhost:5432/library'
-    )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
